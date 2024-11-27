5664b7c90e23bd98c4bc43f1799ed294581d79e3
Eduard Stefes <eduard.stefes@ibm.com>
2024-11-27 16:12:18 +0100

    readded license header

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! # lldb-sys
+//!
+//! [LLDB](https://lldb.llvm.org/) is a debugger from the
+//! [LLVM](https://llvm.org/) project. It is usable as both a
+//! standalone program as well as via an API.
+//!
+//! This crate provides a low-level, fairly direct mapping of the
+//! [public C++ API](https://lldb.llvm.org/cpp_reference/namespacelldb.html)
+//! for LLDB into unsafe Rust.
+//!
+//! It is primarily for use by a higher level crate, such as
+//! [lldb.rs](https://crates.io/crates/lldb/).
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
